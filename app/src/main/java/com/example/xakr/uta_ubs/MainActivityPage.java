03cb1876516e60928daa1abf2c0d5e8fe182f428
sakard23 <sak_4204@yahoo.com>
2016-11-21 18:18:12 -0600

    linked mainpage with club page

--- app/src/main/java/com/example/xakr/uta_ubs/MainActivityPage.java
+++ app/src/main/java/com/example/xakr/uta_ubs/MainActivityPage.java
@@ -16,26 +16,7 @@ public class MainActivityPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-        //clubpage(But);
-        //Button yourButton = (Button) findViewById(R.id.button);
 
-        //yourButton.setOnClickListener(new View.OnClickListener(){
-            /*public void onClick(View v){
-                Intent intent = new Intent ("com.example.xakr.uta_ubs.clubpage");
-                startActivity(intent);
-                //startActivity(new Intent(this, clubpage.class);
-            }
-    public void onClickButtonListener()
-    {
-        join_club = (Button) findViewById(R.id.button);
-        join_club.setOnClickListener(
-                new View.OnClickListener(){
-                    @Override
-            public void onClick (View view){
-                Intent intent = new Intent ("com.example.xakr.uta_ubs.clubpage");
-                startActivity(intent);
-            }
-*/
         Button T=(Button)findViewById(R.id.button4);
         T.setOnClickListener(new View.OnClickListener() {
 
@@ -48,33 +29,3 @@ public class MainActivityPage extends AppCompatActivity {
         });
     }
 }
-
-
-
-
-
-/*
-    public void onClickButtonListener()
-    {
-        join_club = (Button) findViewById(R.id.button);
-        join_club.setOnClickListener(
-                new View.OnClickListener(){
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent = new Intent("com.example.xakr.uta_ubs.clubpage");
-                        startActivity(intent);
-                    }
-
-                    }
-
-        );
-
-    }
-
-
-    public void clubpage(View view) {
-        Intent intent = new Intent (this, clubpage.class);
-        startActivity(intent);
-    }*/
-
-
