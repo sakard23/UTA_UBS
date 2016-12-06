f22718c23c2025f15e995c8cdab8c875bf83d810
sxb0995 <sushil.bista@mavs.uta.edu>
2016-12-06 12:45:37 -0600

    Latest and final

--- app/src/main/java/com/example/xakr/uta_ubs/emailpage.java
+++ app/src/main/java/com/example/xakr/uta_ubs/emailpage.java
@@ -1,25 +1,36 @@
 package com.example.xakr.uta_ubs;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class emailpage extends AppCompatActivity {
-
+public class emailpage extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emailpage);
+
     }
 
-    protected void indivEmail(View view){
-        Intent intent=new Intent(this,indivEmail.class);
-        startActivity(intent);
+    protected void sendEmail(View view){
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{""});
+        i.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
+        i.putExtra(Intent.EXTRA_TEXT, "body of email");
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(emailpage.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+        }
     }
+
 
 
 
